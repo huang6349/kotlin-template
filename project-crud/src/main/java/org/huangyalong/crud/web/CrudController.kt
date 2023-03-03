b7d490efaaca01ad7a75d198a17b8114d2c43266
huang6349 <252683858@qq.com>
2023-03-03 13:59:25 +0800

    :art: 改进代码

--- project-crud/src/main/java/org/huangyalong/crud/web/CrudController.kt
+++ project-crud/src/main/java/org/huangyalong/crud/web/CrudController.kt
@@ -2,6 +2,6 @@ package org.huangyalong.crud.web
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper
 
-interface CrudController<M : BaseMapper<T>?, T> : SaveController<M, T>,
+interface CrudController<M : BaseMapper<T>, T> : SaveController<M, T>,
     QueryController<M, T>,
     DeleteController<M, T>
