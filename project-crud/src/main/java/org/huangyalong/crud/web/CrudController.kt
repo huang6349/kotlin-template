204928c4488253820dd40dd05015ab03e9afaee0
huang6349 <252683858@qq.com>
2023-03-27 20:45:54 +0800

    :art: 改进代码

--- project-crud/src/main/java/org/huangyalong/crud/web/CrudController.kt
+++
@@ -1,7 +0,0 @@
-package org.huangyalong.crud.web
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper
-
-interface CrudController<M : BaseMapper<T>, T> : SaveController<M, T>,
-    QueryController<M, T>,
-    DeleteController<M, T>
---
+++ project-web/src/main/java/org/huangyalong/config/AnylineConfiguration.kt
@@ -0,0 +1,8 @@
+package org.huangyalong.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("org.anyline")
+class AnylineConfiguration
