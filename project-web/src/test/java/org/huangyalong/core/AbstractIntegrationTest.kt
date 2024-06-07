940612fc80830e902a53a1ec806d7d928cfa463a
huang6349 <252683858@qq.com>
2024-06-07 15:05:04 +0800

    :art: 改进代码

--- project-web/src/test/java/org/huangyalong/core/AbstractIntegrationTest.kt
+++ project-web/src/test/java/org/huangyalong/core/AbstractIntegrationTest.kt
@@ -1,9 +1,6 @@
 package org.huangyalong.core
 
-import org.huangyalong.Application
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
 
-@ContextConfiguration(classes = [Application::class])
 @ActiveProfiles("test")
 abstract class AbstractIntegrationTest
