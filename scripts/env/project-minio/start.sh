5b193b2837b3cc860de1bdff93642d0135c690b8
huang6349 <252683858@qq.com>
2024-06-20 09:48:43 +0800

    :art: 改进代码

--- scripts/env/project-minio/start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-WORKDIR=$PWD
-mkdir -p $WORKDIR/data
-chmod -R 777 $WORKDIR/data
-docker-compose -p project-minio up -d --build
--- scripts/env/start.sh
+++ scripts/env/start.sh
@@ -2,6 +2,5 @@
 WORKDIR=$PWD
 cd $WORKDIR/project-adminer/ && sh start.sh
 cd $WORKDIR/project-mysql/ && sh start.sh
-cd $WORKDIR/project-minio/ && sh start.sh
 cd $WORKDIR/project-redis/ && sh start.sh
 docker ps |grep project
